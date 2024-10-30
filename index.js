f18b66e3d2aac054550b23c3fcd6c3f9aa68431e
notobd <notobd4x4@gmail.com>
2024-10-30 12:05:02 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+//selecting elements and assigning them to variables in global scope
+const ratingBtn = document.querySelectorAll('.child');
+console.log(ratingBtn);
+const subBtn = document.querySelector('.sub-but');
+console.log(subBtn);
+const message = document.querySelector('.thank-you');
+console.log(message);
+const hide = document.querySelector('.hidden');
+console.log(hide);
+const overlay = document.querySelector('.overlay');
+console.log(overlay);
+const text = document.querySelector('.selection');
+console.log(text);
+
+// for (let i = 0; i < ratingBtn.length; i++) {
+//   ratingBtn[i].addEventListener("click", function () {
+//     console.log([i]);
+//     text.textContent = `You selected ${i} out of 5`;
+//   });
+// }
+
+//cretaing functionalities
+
+//assigned the ratiingmark in the global scope to be able to access it thru out the progrM
+let ratingMark;
+ratingBtn.forEach(function (btn) {
+  btn.addEventListener('click', function () {
+    ratingMark = btn.textContent;
+  });
+});
+
+//submit button funtion
+const submit = function () {
+  hide.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  text.textContent = `You selected ${ratingMark} out of 5`;
+};
+subBtn.addEventListener('click', submit);
+
+//esc to close overlay
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !hide.classList.contains('.hidden')) {
+    hide.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
